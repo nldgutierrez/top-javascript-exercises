86d4df774b5443ed1e36645247a1841e76ce4705
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2023-02-22 13:27:54 +0800

    Complete Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reversed = "";
+    for (i = word.length-1; i > -1; i--) {
+        reversed += word[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
