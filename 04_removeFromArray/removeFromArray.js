305cbb46cbd41ba0ef0211d6984182b1adc16465
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2023-02-22 14:24:00 +0800

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...item) {
+    for (const each of item) {
+        if (!(array.includes(each))) {
+            continue
+        };
+        array.splice(array.indexOf(each), 1);
+    };
+    return array;
 };
 
 // Do not edit below this line
