997276d55e5366346153f71a1d350cbf8c261a0f
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2023-02-23 16:30:37 +0800

    Complete Exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  // C = (F-32) x (5/9)
+  let celsius;
+  celsius = (fahrenheit - 32) * (5/9);
+  celsius = Math.round(celsius*10)/10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  // F = (C*(9/5)) + 32
+  let fahrenheit;
+  fahrenheit = (celsius * (9/5)) + 32;
+  fahrenheit = Math.round(fahrenheit*10)/10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
