23228477e49f7d778dbd7c398e134f4579ebf337
Noelle Gutierrez <113011161+nldgutierrez@users.noreply.github.com>
2023-02-23 15:02:15 +0800

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if ((start < 0 || end < 0) ||
+        (!Number.isInteger(start) || !Number.isInteger(end))) {
+        return "ERROR";
+    };
+    
+    if (start > end) {
+        let holder = start;
+        start = end;
+        end = holder;
+    };
+    
+    let sum = 0;
+    for (i = start; i <= end; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
